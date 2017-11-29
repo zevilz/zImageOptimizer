29043c57209cb480611c0f197a938fe9ef8482fe
zEvilz <zevilz13@yandex.ru>
2017-11-29 22:01:16 +0500

    updated

--- zImageOptimizer.sh
+++ zImageOptimizer.sh
@@ -376,7 +376,7 @@ installDeps()
 				rm pngout-20150319-bsd.tar.gz
 				if [ $PLATFORM_ARCH == 64 ]
 				then
-					$SUDO cp pngout-20150319-linux/x86_64/pngout /bin/pngout
+					$SUDO cp pngout-20150319-linux/amd64/pngout /bin/pngout
 				else
 					$SUDO cp pngout-20150319-linux/i686/pngout /bin/pngout
 				fi
